4477c48f7edd36c9e5c0eae6738edb3228e3fbc9
ALaws0n <106745609+ALaws0n@users.noreply.github.com>
2023-02-19 22:56:48 -0500

    Added comments and result formatting

--- scripts/robot_config.py
+++ scripts/robot_config.py
@@ -1,57 +1,81 @@
-# import these incase I need 'em
+# Import important modules
 import math
 import robot_model as rm
 
 
 def two_link_planar_manipulator():
+    """
+    Test case for a two link planar manipulator with the following joint conditions:
+        Joint 1: Theta = pi/2, a = 1, d = 0, alpha = 0
+        Joint 2: Theta = pi/2, a = 1, d = 0, alpha = 0
+    :return: None
+    """
+    # Create a 2D array of Denavit-Hartenberg parameters for each joint
     dh_parameters = [[(math.pi/2), 1, 0, 0], [(math.pi/2), 1, 0, 0]]
+    # Get the homogenous transformation matrix from passing the dh_parameters array to the kinematic chain function
     homogenous_transformation = rm.kinematic_chain(dh_parameters)
+    # Get the x, y, z components from the homogenous transformation matrix
     x, y, z = rm.get_pos(homogenous_transformation)
+    # Get the roll, pitch, yaw angles from the homogenous transformation matrix
     roll, pitch, yaw = rm.get_rot(homogenous_transformation)
-
-    print('x: ', x, '\nY: ', y, '\nZ: ', z)
-    print("Roll: ", roll)
-    print("Pitch: ", pitch)
-    print("Yaw: ", yaw)
+    # Display the results from the calculations
+    print(f'X: {x:.2f} meters\nY: {y:.2f} meters\nZ: {z:.2f} meters\n'
+          f'Roll: {roll:.2f} radians\nPitch: {pitch:.2f} radians\nYaw: {yaw:.2f} radians')
 
 
 def UR5e_case_1():
+    """
+    UR5e Test Case 1 using given Denavit-Hartenberg Parameters and a joint angle of 0 for each joint
+    :return: None
+    """
+    # Create 2D array of Denavit-Hartenberg parameters for each joint
     dh_parameters = [[0, 0, 0.1625, math.pi/2],   # Joint 1
                      [0, -0.425, 0, 0],           # Joint 2
                      [0, -0.3922, 0, 0],          # Joint 3
                      [0, 0, 0.1333, math.pi/2],   # Joint 4
                      [0, 0, 0.0998, -math.pi/2],  # Joint 5
                      [0, 0, 0.0996, 0]]           # Joint 6
+
+    # Get the full homogenous transformation matrix of the kinematic chain
     homogenous_transformation = rm.kinematic_chain(dh_parameters)
+    # Get the x, y, z components of the homogenous transformation matrix
     x, y, z = rm.get_pos(homogenous_transformation)
+    # Get the roll, pitch, yaw angles of the homogenous transformation matrix
     roll, pitch, yaw = rm.get_rot(homogenous_transformation)
-    print('x: ', x, '\nY: ', y, '\nZ: ', z)
-    print("Roll: ", roll)
-    print("Pitch: ", pitch)
-    print("Yaw: ", yaw)
+    # Display the results from the calculations
+    print(f'X: {x:.2f} meters\nY: {y:.2f} meters\nZ: {z:.2f} meters\n'
+          f'Roll: {roll:.2f} radians\nPitch: {pitch:.2f} radians\nYaw: {yaw:.2f} radians')
 
 
 def UR5e_case_2():
+    """
+    UR5e Test Case 1 using given Denavit-Hartenberg Parameters and a joint angle of 0 all joints except Joint 2.
+    Joint 2 will be given an angle of -pi/2
+    :return: None
+    """
+    # Create 2D array of Denavit-Hartenberg parameters for each joint
     dh_parameters = [[0, 0, 0.1625, math.pi/2],   # Joint 1
-                     [-math.pi/2, -0.425, 0, 0],           # Joint 2
+                     [-math.pi/2, -0.425, 0, 0],  # Joint 2
                      [0, -0.3922, 0, 0],          # Joint 3
                      [0, 0, 0.1333, math.pi/2],   # Joint 4
                      [0, 0, 0.0998, -math.pi/2],  # Joint 5
                      [0, 0, 0.0996, 0]]           # Joint 6
+    # Get the full homogenous transformation matrix of the kinematic chain
     homogenous_transformation = rm.kinematic_chain(dh_parameters)
+    # Get the x, y, z components of the homogenous transformation matrix
     x, y, z = rm.get_pos(homogenous_transformation)
+    # Get the roll, pitch, yaw angles of the homogenous transformation matrix
     roll, pitch, yaw = rm.get_rot(homogenous_transformation)
-    print('x: ', x, '\nY: ', y, '\nZ: ', z)
-    print("Roll: ", roll)
-    print("Pitch: ", pitch)
-    print("Yaw: ", yaw)
+    # Display the results from the calculations
+    print(f'X: {x:.2f} meters\nY: {y:.2f} meters\nZ: {z:.2f} meters\n'
+          f'Roll: {roll:.2f} radians\nPitch: {pitch:.2f} radians\nYaw: {yaw:.2f} radians')
 
 
 if __name__ == '__main__':
     print("Two Link Planar Manipulator")
     two_link_planar_manipulator()
-    print("\n UR5e Case 1")
+    print("\nUR5e Case 1")
     UR5e_case_1()
-    print("\n UR5e Case 2")
+    print("\nUR5e Case 2")
     UR5e_case_2()
 
--- scripts/robot_model.py
+++ scripts/robot_model.py
@@ -4,34 +4,54 @@ import math
 
 
 def dh_transformation(theta, a, d, alpha):
-    # Receives denvait-hartenburg parameters
-    # returns a combined homogenous transformation matrix according to dh convention
-    Ai = np.array(
+    """
+    Receives Denavit-Hartenberg parameters and returns a combined homogenous transformation matrix
+
+    :param theta: Joint Angle
+    :param a: Link Length
+    :param d: Link Offset
+    :param alpha: Link Twist
+    :return: Homogenous transformation matrix
+    """
+
+    # Create a transformation matrix using the provided Denavit-Hartenberg parameters
+    transformation_matrix = np.array(
         [[math.cos(theta), -math.sin(theta) * math.cos(alpha), math.sin(theta) * math.sin(alpha), a * math.cos(theta)],
          [math.sin(theta), math.cos(theta) * math.cos(alpha), -math.cos(theta) * math.sin(alpha), a * math.sin(theta)],
          [0, math.sin(alpha), math.cos(alpha), d],
          [0, 0, 0, 1]])
+    # Return transformation matrix
+    return transformation_matrix
 
-    return Ai
 
-
-def kinematic_chain(link_chain):
-    # receives a 2D array containing dh parameters
-    # return a homogenous transformation
-    # loop that scans the rows and multiplies the transformation
-    # init with an identity matrix
+def kinematic_chain(joint_chain):
+    """
+    Receives a 2D array containing the Denavit-Hartenberg parameters for each joint
+    Returns a homogenous transformation matrix for the entire kinematic chain
+    :param joint_chain: 2D array of Denavit-Hartenberg parameters
+    :return: Homogenous Transformation Matrix
+    """
+    # Initialize running total with an Identity Matrix
     homogenous_transformation = np.array([[1, 0, 0, 0],
                                           [0, 1, 0, 0],
                                           [0, 0, 1, 0],
                                           [0, 0, 0, 1]])
-    for link in link_chain:
-        dh_trans_result = dh_transformation(link[0], link[1], link[2], link[3])
+    # Loop through each set of joint parameters in the chain
+    for joint in joint_chain:
+        # Pass the Denavit-Hartenberg Parameters to the dh_transformation function
+        dh_trans_result = dh_transformation(joint[0], joint[1], joint[2], joint[3])
+        # Multiply the resulting Matrix by the running total
         homogenous_transformation = np.matmul(homogenous_transformation, dh_trans_result)
-
+    # Return homogenous transformation matrix for the entire kinematic chain
     return homogenous_transformation
 
 
 def get_pos(transformation_matrix):
+    """
+    Receives a homogenous transformation matrix and returns the x, y, z components
+    :param transformation_matrix: Homogenous Transformation Matrix
+    :return: x, y, z components
+    """
     # receives a homogenous transformation and returns x,y,z components
     x = transformation_matrix[0][3]
     y = transformation_matrix[1][3]
@@ -41,12 +61,18 @@ def get_pos(transformation_matrix):
 
 
 def get_rot(transformation_matrix):
-    # receives homogenous transformation matrix
-    # returns roll-pitch-yaw angles
-    # TODO - look at the Pitch formula some more if this doesnt work
+    """
+    Receives a homogenous transformation matrix and returns the roll-pitch-yaw angles
+    :param transformation_matrix: Homogenous transformation matrix
+    :return: Roll-Pitch-Yaw angles
+    """
+    # Get Roll angle using given formula
     roll = (math.atan2((transformation_matrix[2][1]), (transformation_matrix[2][2])))
+    # Get Pitch angle using given formula
     pitch = (math.atan2(-(transformation_matrix[2][0]),
                         math.sqrt((transformation_matrix[2][1]) ** 2 + (transformation_matrix[2][2]) ** 2)))
+    # Get Yaw angle using given formula
     yaw = (math.atan2((transformation_matrix[1][0]), (transformation_matrix[0][0])))
 
+    # Return roll, pitch, yaw angles
     return roll, pitch, yaw
